57500bf25bd6f4d209b7ad69d2ba4a86982436d4
fafale <olhoender@gmail.com>
2025-12-03 23:35:32 -0300

    first examples/attempts to get loc id via name

---
+++ main.gd
@@ -0,0 +1,39 @@
+extends Control
+
+@onready var node_ip = $ConnectionInfo/LineditIP
+@onready var node_port = $ConnectionInfo/LineditPORT
+@onready var node_slot = $ConnectionInfo/LineditSLOT
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_bt_connect_pressed() -> void:
+	Archipelago.ap_connect(node_ip.text, node_port.text, node_slot.text)
+
+
+func _on_bt_exec_pressed() -> void:
+	if false:
+		print(Archipelago.conn.slot_data["area_games"]) # dict with everything
+	
+	# load slot info
+	var data = Archipelago.conn.get_gamedata_for_player()
+	
+	print(Archipelago.location_list()) # all available ids
+	
+	# return the loc name
+	print(data.get_loc_name(4))
+	print(data.get_loc_name(20))
+	print(data.get_loc_name(36))
+	print(data.get_loc_name(40))
+	
+	# return the loc id (access by name), or -1 if don't exist
+	print(data.get_loc_id("Unlock: The Cloaked Entrance"))
+	
+	pass # Replace with function body.
