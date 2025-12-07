66b0d1d068c0cf8387cf68472a56928f046df6b9
fafale <olhoender@gmail.com>
2025-12-07 15:45:39 -0300

    set basic connection menu

--- main.gd
+++ main.gd
@@ -1,8 +1,8 @@
 extends Control
 
-@onready var node_ip = $Connection/WindowConnection/LineditIP
-@onready var node_port = $Connection/WindowConnection/LineditPORT
-@onready var node_slot = $Connection/WindowConnection/LineditSLOT
+@onready var node_ip: LineEdit = $Connection/WindowConnection/LineditIP
+@onready var node_port: LineEdit = $Connection/WindowConnection/LineditPORT
+@onready var node_slot: LineEdit = $Connection/WindowConnection/LineditSLOT
 
 @onready var disconnect_warning = $DisconnectWarning
 
@@ -17,13 +17,24 @@ func _ready() -> void:
 
 
 func _on_bt_connect_pressed() -> void:
-	Archipelago.connected.connect(kmk.initialize_kmk.unbind(1))
-	Archipelago.connected.connect(failsafe_connect.unbind(2))
-	$Connection.hide()
+	var able_to_connect = true
+	if node_ip.text.is_empty():
+		able_to_connect = false
+	if not node_port.text.is_valid_int():
+		able_to_connect = false
+	if node_slot.text.is_empty():
+		able_to_connect = false
 	
-	await get_tree().create_timer(1.0).timeout
-	
-	Archipelago.ap_connect(node_ip.text, node_port.text, node_slot.text)
+	if able_to_connect:
+		Archipelago.connected.connect(kmk.initialize_kmk.unbind(1))
+		Archipelago.connected.connect(failsafe_connect.unbind(2))
+		
+		$Connection.hide()
+		$Client.show()
+		
+		await get_tree().create_timer(1.0).timeout
+		
+		Archipelago.ap_connect(node_ip.text, node_port.text, node_slot.text)
 
 # Failsafe against bad/first connections
 func failsafe_connect() -> void:
